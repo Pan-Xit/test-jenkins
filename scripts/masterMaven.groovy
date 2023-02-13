e066fff3757da27a4713e79f05498e34d24ff9b6
lx-romanborysiuk <78418809+lx-romanborysiuk@users.noreply.github.com>
2023-02-13 19:43:24 +0200

    Update masterMaven.groovy

--- scripts/masterMaven.groovy
+++ scripts/masterMaven.groovy
@@ -1,7 +1,7 @@
 mavenJob('example') {
   preBuildSteps {
     remoteTrigger('my_host', 'pipeline_with_params') {
-      parameterFile("${WORKSPACE}scripts/test.parameters")
+      parameterFile("${WORKSPACE}/scripts/test.parameters")
       blockBuildUntilComplete()
       shouldNotFailBuild(true)
     }
