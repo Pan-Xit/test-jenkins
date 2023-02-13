6dcf6cdc6fbd46ae604ead2add1434e41838d248
lx-romanborysiuk <78418809+lx-romanborysiuk@users.noreply.github.com>
2023-02-13 18:51:34 +0200

    Update masterMaven.groovy

--- scripts/masterMaven.groovy
+++ scripts/masterMaven.groovy
@@ -1,8 +1,4 @@
 mavenJob('example') {
-  environmentVariables {
-    propertiesFile("scripts/test.properties")
-  }
-  
   preBuildSteps {
     remoteTrigger('my_host', 'pipeline_with_params') {
       parameterFile("scripts/test.parameters")
